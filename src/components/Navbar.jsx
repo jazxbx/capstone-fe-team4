2c0272b954213755db52facc3413f7b03d393ec0
jazz <jazxbx@gmail.com>
2024-05-03 20:29:47 -0700

    initial nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import chef from "../assets/chef-icon.png";
-import "./navbarCss.css";
-import { useNavigate } from "react-router-dom";
+import styles from './Navbar.module.css';
 
-export default function Navbar({ user, isLoggedIn, logout }) {
-  const navigate = useNavigate();
+export function Navbar() {
   return (
-    <div className="nav-container">
-<div className="icon-container">
-  <a href="/" onClick={(e) => { e.preventDefault(); navigate('/'); }} className="no-underline">
-    <div className="icon-text-container">
-      <img src={chef} style={{ width: "50px" }} alt="chef-icon" />
-      <h1>TasteBUD</h1>
-    </div>
-  </a>
-</div>
-
-      <div className="nav-login" style={{ display: "flex", gap: "30px" }}>
-        {isLoggedIn ? (
-          <button onClick={logout} style={{ width: "100px", height: "30px" }}>
-            Logout
-          </button>
-        ) : (
-          <button
-            onClick={() => navigate("/login")}
-            style={{ width: "100px", height: "30px" }}
-          >
-            Login
-          </button>
-        )}
+    <div className={styles.navContainer}>
+      <p className={styles.app}>tastebud</p>
+      <div className={styles.flex}>
+        <div>theme</div>
+        <div>icon</div>
+        <div>menu</div>
       </div>
     </div>
   );
