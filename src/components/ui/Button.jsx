83128eacfe6ebcc4865878d283698c9132b24423
jazz <jazxbx@gmail.com>
2024-05-04 13:21:05 -0700

    add button ui

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,3 +1,9 @@
-export function Button() {
-  return <div>Button</div>;
+import styles from './Button.module.css';
+
+export function Button({ variant = 'default', children, onClick }) {
+  return (
+    <button onClick={onClick} className={styles[variant]}>
+      {children}
+    </button>
+  );
 }
