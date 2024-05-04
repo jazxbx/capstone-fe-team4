0da952ed3684958b2c4c5c0bfc1de752e2f4a1d4
jazz <jazxbx@gmail.com>
2024-05-04 16:27:52 -0700

    add initial footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { Header } from './components/Header';
 import { Navbar } from './components/Navbar';
 import { Search } from './components/Search';
+import { Footer } from './pages/Footer';
 
 export function App() {
   return (
-    <div className='container'>
-      <Header />
-      <Navbar />
-      <main className='main'>
-        <div className='searchContainer'>
-          <Search />
-        </div>
-        <div>Hello this is APP</div>
-        <div>Test div</div>
-      </main>
-    </div>
+    <>
+      <div className='container'>
+        <Header />
+        <Navbar />
+        <main className='main'>
+          <div className='searchContainer'>
+            <Search />
+          </div>
+          <div>Hello this is APP</div>
+          <div>Test div</div>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
